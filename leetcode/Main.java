2061000f50a8b5ed2f454cfc5aab2f6c01c8c51e
macbook <sivlaien@icloud.com>
2020-12-16 23:23:29 +0300

    Changed LeetcodeSourceData.java, Main.java and ReadWriteUtils.java, added streams and removed unnecessary things

--- leetcode/Main.java
+++ leetcode/Main.java
@@ -1,11 +1,10 @@
 import java.io.File;
-import java.util.List;
 
 public class Main{
     private static final String filename = "source_leetcode_data.txt";
     public static void main(String[] args) {
         String workingDirectory = System.getProperty("user.dir");
-        List<LeetcodeSourceData> data = ReadWriteUtils.read(workingDirectory + File.separator + filename);
-        ReadWriteUtils.write("intervals.md", data);
+        LeetcodeSourceData data = ReadWriteUtils.read(workingDirectory + File.separator + filename);
+        ReadWriteUtils.write(args[0], data);
     }
 }
--- leetcode/ReadWriteUtils.java
+++ leetcode/ReadWriteUtils.java
@@ -3,23 +3,17 @@ import java.util.*;
 
 
 public class ReadWriteUtils {
-    public static List<LeetcodeSourceData> read(String fileName) {
-        List<List<String>> dataArray = new ArrayList<>();
+    public static LeetcodeSourceData read(String fileName) {
+        List<String> dataList = new ArrayList<>();
         try {
             String dataString = new Scanner(new File(fileName)).useDelimiter("\\Z").next();
-            for (String oneTask : dataString.split("split_word\n")) {
-                dataArray.add(Arrays.asList(oneTask.split("\n")));
-            }
+            Collections.addAll(dataList, dataString.split("\n"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        List<LeetcodeSourceData> newFormatData = new ArrayList<>();
-        for (List<String> task: dataArray) {
-            newFormatData.add(new LeetcodeSourceData(task));
-        }
-        return newFormatData;
+        return new LeetcodeSourceData(dataList);
     }
-    public static void write(String fileName, List<LeetcodeSourceData> data) {
+    public static void write(String fileName, LeetcodeSourceData data) {
         try {
             FileWriter writer = new FileWriter(fileName);
             writer.write("# ");
@@ -27,12 +21,8 @@ public class ReadWriteUtils {
             writer.write(allTaskTitle.substring(0, 1).toUpperCase());
             writer.write(String.join(" ", allTaskTitle.substring(1, allTaskTitle.indexOf(".")).split("-")));
             writer.write("\n\n");
-            for (LeetcodeSourceData task: data) {
-                writer.write(task.getMdSubtitle());
-            }
-            for (LeetcodeSourceData task: data) {
-                writer.write(task.getMdTask());
-            }
+            writer.write(data.getMdSubtitle());
+            writer.write(data.getMdTask());
             writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
